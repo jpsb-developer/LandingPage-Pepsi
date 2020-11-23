c0b6e59ac3b4ee80acbe0127441a0cc25dc8ba39
João Paulo Batista <jovemuchiha@hotmail.com>
2020-11-23 20:24:42 -0300

    finished project

---
+++ main.js
@@ -0,0 +1,11 @@
+const icons = document.querySelector('.small-image');
+const body = document.querySelector('body');
+
+
+const imgSlider = (src,color) =>{
+    const image = document.querySelector('.pepsi').src=src;
+    body.style.background = `${color}`;
+}
+
+
+
